74235384f9746395ea7c4197e9612eccab2457b3
Benjamin Phillips <benjamingt537@gmail.com>
2024-04-02 13:43:47 -0400

    got API fetch working

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world!")
+import requests
+
+# URL = 'https://api.wynncraft.com/public_api.php?action=itemDB&category={"armor"}'
+URL = 'https://api.wynncraft.com/v2/player/BENNNNZO/stats'
+
+def main():
+    x = requests.get(URL)
+    print(x.json())
+
+if __name__ == "__main__":
+    main()
