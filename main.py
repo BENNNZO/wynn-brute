6891c94e4705eb5ee3b517cbbd79dfda52890052
Benjamin Phillips <benjamingt537@gmail.com>
2024-04-02 14:43:36 -0400

    fetched item API and formatted json to items.json

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-import requests
+import requests, json
 
-# URL = 'https://api.wynncraft.com/public_api.php?action=itemDB&category={"armor"}'
-URL = 'https://api.wynncraft.com/v2/player/BENNNNZO/stats'
+URL = 'https://api.wynncraft.com/v3/item/database?fullResult'
 
 def main():
-    x = requests.get(URL)
-    print(x.json())
+    lb()
+    try:
+        data = requests.get(URL)
+
+        f = open("./json/items.json", "w", encoding="utf-8")
+        f.write(str(json.dumps(data.json(), indent=4)))
+        f.close()
+        
+        print(" >>> Successfully Fetched & Wrote Data!")
+    except:
+        print(" >>> Failed To Get Data :(")
+    lb()
+
+def lb():
+    print("------------------------------------------------------------------")
 
 if __name__ == "__main__":
     main()
